9682b3ea6b3acf9281327b9ddb373bb58c35b193
sindhuula <sindhuula@gmail.com>
2015-05-04 13:51:11 -0400

    Finding Combinations

--- try4.java
+++ try4.java
@@ -1,32 +1,7 @@
 package trial;
 
-import java.awt.List;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-class variable
-{
-public int count;
-//public String[][] result;
-variable(int a)
-{
-this.count = a;	
-//this.result[a] = null;
-}
-/*
-public void equate(String[] res, int n)
-{
-	result[n]=res;
-}*/
-public void add()
-{
-this.count ++;	
-}
-public void print()
-{
-	System.out.println("Counter is "+this.count);
-}
-}
 public class try4 {
     public static void main(String[] args) {
         String[] arr = {"A","B","C","D","E"};
@@ -37,50 +12,37 @@ public class try4 {
     private static void combine(String[] arr, int r) {
         String[] res = new String[r];
         ArrayList<String[]> result = new ArrayList<String[]>();
-        variable v = new variable(-1);
-        doCombine(arr, res, 0, 0, r,v,result);
-        System.out.println("res\n"+res[0]+res[1]+res[2]);
-        System.out.println("\nIn main"+result.size());
-        //Trying to print the result
-        for (Object[] array : result)
+        ArrayList<String[]> answer = new ArrayList<String[]>();
+        answer = doCombine(arr, res, 0, 0, r,result);
+        System.out.println("\nIn main "+result.size());
+        for (Object[] array : answer)
         {
         	  for (Object o : array)
         	    System.out.print("item: " + o);    
         		System.out.println();
         	}
     }
-        private static void doCombine(String[] arr, String[] res, int currIndex, int level, int r,variable counter, ArrayList<String[]> result) {
+        private static ArrayList<String[]> doCombine(String[] arr, String[] res, int currIndex, int level, int r, ArrayList<String[]> result) {
         
         if(level == r){
-            printArray(res);
-     //http://stackoverflow.com/questions/18277657/creating-arraylist-of-arrays - We have similar pblm
             String[] inter = new String[r];
-            inter = res;
+            System.arraycopy(res,0,inter,0,r);
             result.add(inter);
-            //Tryinh to see wht the array list has every time
-            for (Object[] array : result)
-          	  {
-            	for (Object o : array)
-          	    {System.out.print("item: " + o);}
-            	System.out.println();
-          	  }
-           inter = null; 
-            return;
+            inter = null; 
+            return result;
      	}
        for (int i = currIndex; i < arr.length; i++)
         {
-    	   counter.add();
-           res[level] = arr[i];
-//           System.out.print(result.size());
-       	   doCombine(arr, res, i+1, level+1, r,counter, result);//,result);        
+    	  res[level] = arr[i];
+      	  doCombine(arr, res, i+1, level+1, r, result);//,result);        
         }
+	return result;
     }
 
-    private static String[] printArray(String[] res) {
+    private static void printArray(String[] res) {
         for (int i = 0; i < res.length; i++) {
             System.out.print(res[i] + " ");
         }
         System.out.println();
-        return res;
     }
 }
